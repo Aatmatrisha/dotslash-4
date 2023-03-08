d46a65f3cc8b1caaccd92af298e0ab35a1af2905
Sathvik Murthy <sathvikmurthy31@gmail.com>
2023-03-08 23:42:29 +0530

    hero section with mobile view, nav

--- components/Nav.js
+++ components/Nav.js
@@ -3,25 +3,27 @@ import Image from "next/image";
 import Link from "next/link";
 import PesLogo from "../public/assets/pes_logo.svg";
 import AtLogo from "../public/assets/at_logo.svg";
+import BurgerIcon from "../public/assets/burger_icon.svg";
 
 export default function Nav() {
     return (
-        <nav className="w-full bg-red-500 flex flex-row h-[80px] fixed z-50">
-            <div className="hidden md:w-full md:h-full md:flex-row md:items-center md:flex md:flex-row md:items-center md:pl-[20px] md:pr-[20px] md:justify-between md:bg-red-600">
-                <Link href="/">
-                    <Image src={PesLogo} alt="PES University" />
+        <nav className="w-full flex flex-row h-[80px] fixed z-50">
+            <div className="hidden md:w-full md:h-full md:flex-row md:items-center md:flex md:flex-row md:items-center md:pl-[20px] md:pr-[20px] md:justify-between">
+                <Link className="h-full flex flex-col justify-center" href="/">
+                    <Image className="h-[90%]" priority src={PesLogo} alt="PES University" />
                 </Link>
-                <div className="flex flex-row items-center gap-5">
-                    <Link href="/">HOME</Link>
-                    <Link href="/">SPONSORS</Link>
-                    <Link href="/">AT23</Link>
+                <div className="flex flex-row items-center gap-20 font-Mouse text-white">
+                    <Link className="hover:text-secondary" href="/">HOME</Link>
+                    <Link className="hover:text-secondary" href="/">SPONSORS</Link>
+                    <Link className="hover:text-secondary" href="/">AT23</Link>
+                    <Link className="hover:text-secondary" href="/register">REGISTER</Link>
                 </div>
-                <Image src={AtLogo} alt="Aatmatrisha 2023" />
+                <Image className="h-[90%]" priority src={AtLogo} alt="Aatmatrisha 2023" />
             </div>
-            <div className="flex flex-row items-center w-full pl-[20px] pr-[20px] justify-between md:hidden">
-                <button><Image alt="Menu" /></button>
-                <Image alt="Aatmatrisha 2023" />
-                <Image alt="PES University" />
+            <div className="flex relative flex-row items-center w-full justify-center md:hidden">
+                <button className="absolute left-[20px]"><Image className="h-[90%]" priority src={BurgerIcon} alt="Menu" /></button>
+                <Image className="h-[80%]" priority src={AtLogo} alt="Aatmatrisha 2023" />
+                <Image className="h-[80%] absolute right-[5px]" priority src={PesLogo} alt="PES University" />
             </div>
         </nav>
     )
--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,44 @@ import styles from '@/styles/Home.module.css'
 import Image from "next/image";
 import GlobeImage from "../public/assets/hero_globe.svg";
 import DotslashLogo from "../public/assets/dotslash_logo.svg";
+import Vector1 from "../public/assets/hero_img1.svg";
+import Vector2 from "../public/assets/hero_img2.svg";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      <div className="flex flex-col h-screen w-full bg-red-500 items-center justify-center relative">
-        <Image src="" alt="" />
-        <Image src="" alt="" />
-        <Image className="absolute left-[20px] top-[20%]" src={DotslashLogo} alt="" />
-        <Image className="absolute w-full" src={GlobeImage} alt="" />
+    <div className="flex flex-col min-h-screen w-full overflow-hidden">
+      <div className="flex flex-col h-screen w-full bg-primary items-center justify-center relative">
+        <Image className="absolute bottom-[10%] left-[-30px] h-60 w-60 md:bottom-[15%]" priority src={Vector1} alt="Vector1" />
+        <Image className="absolute top-[10%] right-[-40px] h-48 w-48 md:top-[15%]" priority src={Vector2} alt="Vector2" />
+        <Image className="absolute left-[10%] top-[20%] h-20 w-20" priority src={DotslashLogo} alt="DotSlash" />
+        <Image className="absolute w-full md:h-[90%]" priority src={GlobeImage} alt="Globe" />
         <div className="flex flex-col items-center justify-center gap-5 absolute">
-          <h1 className="font-Redhat text-7xl font-extrabold text-white select-none">dotSlash4<span className="text-secondary">.</span>0</h1>
-          <h2 className="text-white font-Poppins font-semibold text-xl">HACKUS POCKUS</h2>
+          <h1 className="font-Redhat text-7xl font-extrabold text-white select-none md:text-9xl">dotSlash4<span className="text-secondary">.</span>0</h1>
+          <h2 className="text-white font-Poppins font-semibold text-xl select-none md:text-2xl">HACKUS POCKUS</h2>
         </div>
+      </div>
+      {/* hero ends here */}
+
+      <div>
+        <h2>SPONSORS</h2>
+        <div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+      {/* sponsors section ends here */}
+
+      <div>
+        <Image src="" alt="Vector3" />
+        <div>
+          <h2>What is dotSlash</h2>
+          <p>Lorem ipsum dolor</p>
+        </div>
+        <Image src="" alt="Vector4" />
       </div>
     </div>
   )
